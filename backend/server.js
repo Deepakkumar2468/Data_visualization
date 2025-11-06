01955236882f5448b7e06adcc005866e97a7db4c
Deepakkumar2468 <124360840+Deepakkumar2468@users.noreply.github.com>
2025-11-06 20:50:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -49,8 +49,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-// const PORT = process.env.PORT || 5000;
-const PORT = 5050;
+const PORT = process.env.PORT || 5000;
+// const PORT = 5050;
 
 app.listen(PORT, () => {
   console.log(`✓ Server running on http://localhost:${PORT}`);
